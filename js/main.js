6d5ad70b67c72d821acdf7de8b5234c8df51bc8b
Alekasandr <ya@inwiz.ru>
2015-06-26 03:39:27 +0500

    search

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 	$('.js-custom-form-el').styler();
+    $('.valanter-full-link').click(function(){
+      $('#valanterModal').arcticmodal();
+      return false;
+    });
+    $('.priut-full-link').click(function(){
+      $('#valanterModal').arcticmodal();
+      return false;
+    });
+
 });
